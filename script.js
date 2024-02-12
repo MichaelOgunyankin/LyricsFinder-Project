a123f6312a7164594456eb9ca27d9ac199b48590
MichaelOgunyankin <149902350+MichaelOgunyankin@users.noreply.github.com>
2024-02-12 16:47:16 +0000

    Structured the boilerplates, added initial heading, input and button
    Added fonts and text align
    
    Added documents to script via DOM

---
+++ script.js
@@ -0,0 +1,8 @@
+//add html elements:
+
+//results output
+const searchResults = document.getElementById('div3')
+//searchbar 
+const searchbar = document.getElementById('searchBar')
+//searchButton
+const searchButton = document.getElementById('searchbtn')
