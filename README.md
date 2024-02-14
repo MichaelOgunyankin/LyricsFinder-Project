66f4a92f6566163a1661f3fdb74fc3811c22f1e5
MichaelOgunyankin <149902350+MichaelOgunyankin@users.noreply.github.com>
2024-02-14 15:22:35 +0000

    API addition API added url and api data variables for artist and song searched formed element to append to searchResults div formed

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # JS-Project
  Project Week Task
+
+
+
+
+    width: 200px;
+    margin-bottom: 20px;
+    border-left: none;
+    border-top: none;
+    border-right: none;
+    border-bottom: 2x solid black;
+    display: block;
+  /*  border-radius: ; */
--- script.js
+++ script.js
@@ -6,3 +6,47 @@ const searchResults = document.getElementById('div3')
 const searchbar = document.getElementById('searchBar')
 //searchButton
 const searchButton = document.getElementById('searchbtn')
+
+//inputs
+const artistInput = document.getElementById('artistInput') 
+const songInput = document.getElementById('songInput')
+
+
+
+
+
+// Asynchronous function to fetch the data
+async function getData(artist, song) {
+    // Define the api endpoint we want to fetch from
+    let apiURL = 'https://api.lyrics.ovh/v1/';
+    // Await the response from the API
+    const response = await fetch(apiURL+artist+"/"+song);
+    // Convert the response to JSON
+    const apiData = await response.json();
+    // Return the JSON response
+    return apiData;
+}
+
+searchButton.addEventListener('click', async () => {
+    // Create an array to store the apiData
+    
+    let artist = artistInput.value
+    let song = songInput.value
+    let apiData = [];
+    // Run the getData() function in a try/catch in order to detect errors
+    try {
+        apiData = await getData(artist, song);
+    } catch (error) {
+        console.log(error);
+    }
+    // Log the data received from the api
+    //console.log(apiData);
+
+    const lyricsText = document.createElement('p');
+    lyricsText.innerText = JSON.stringify(apiData);
+    lyricsText.classList.add('lyricsText');
+    searchResults.append(lyricsText);
+    
+});
+
+//console.log(artistInput)
